7d56a859bacad2d07afb1a28fc70ae73683006f9
KarineMomose <86853947+KarineMomose@users.noreply.github.com>
2021-07-25 11:17:23 -0300

    Adicionado pasta Build

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Home from './view/Home';
 
 const App: React.FC = () => {
   fetch(process.env.REACT_APP_API_URL || 'http://localhost:8080/api/v1')
-    .then(() => console.log('OK!!!'))
+    .then(() => console.log('OK!'))
     .catch(() => console.log('NOK...'))
   return <Home />;
 }
